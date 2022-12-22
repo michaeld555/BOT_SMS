923ae67a06a251905bf27bf7313edc9624f90950
Michael Douglas <usermich999@gmail.com>
2022-12-22 18:32:02 -0300

    #6

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,16 @@
 import TelegramBot from 'node-telegram-bot-api';
 import dotenv from 'dotenv';
+import mysql from 'mysql2';
 
 dotenv.config();
 
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+  });
+
 const bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true});
 
-export { bot };
+export { bot, connection };
